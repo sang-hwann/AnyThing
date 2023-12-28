d7c53147ff275506b7654913334ef00ae6ee0ecf
skyi0909@naver.com <skyi0909@naver.com>
2023-12-29 00:20:33 +0900

    프로젝트 파일 생성 ,Initial commit

---
+++ src/main/java/com/repayment/anything/AnyThingApplication.java
@@ -0,0 +1,13 @@
+package com.repayment.anything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyThingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnyThingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/repayment/anything/AnyThingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repayment.anything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyThingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
